4e6f08d670cd29902bea642bf7a62ad0f11c4c32
FabienCampredon <campredon.fab@hotmail.fr>
2017-11-14 17:10:59 +0100

    Update config file

--- src/test/java/fr/elfoa/hello/jpa/CrmJPATest.java
+++ src/test/java/fr/elfoa/hello/jpa/CrmJPATest.java
@@ -1,10 +1,50 @@
 package fr.elfoa.hello.jpa;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 /**
  * @author CAMPREDON & CHOMONT
  */
 
 public class CrmJPATest {
 
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("crm--database");
+    private EntityManager em;
+    private EntityTransaction tx;
 
+    @Before
+    public void initEntityManager() throws Exception {
+        em = emf.createEntityManager();
+        tx = em.getTransaction();
+    }
+
+
+    @After
+    public void closeEntityManager() throws Exception {
+        if (em != null) {
+            em.close();
+        }
+    }
+
+    @Test
+    public void test(){
+
+
+        A a = new A("A1","A1");
+
+        tx.begin();
+        em.persist(a);
+        tx.commit();
+
+        Assert.assertEquals(1,a.getId().intValue());
+
+    }
 }
