96062bff133520d80b5295bf2af9e6a9ad3b58c0
Pierre Colomb <pierre.colomb@braincube.com>
2018-10-17 17:01:36 +0200

    Fix PairTest

--- src/test/java/fr/elfoa/sample/PairTest.java
+++ src/test/java/fr/elfoa/sample/PairTest.java
@@ -7,18 +7,18 @@ import static org.junit.Assert.*;
 /**
  * @author Pierre Colomb
  */
-public class PointTest {
+public class PairTest {
 
 
     @Test
     public void testConstruction(){
 
-        Point point = new Point(1,2);
+        Pair point = new Pair(1, 2);
 
         assertEquals(1,point.getX());
         assertEquals(2,point.getY());
 
-        Point other = new Point(1,2);
+        Pair other = new Pair(1, 2);
 
         assertTrue(other.equals(point));
     }
